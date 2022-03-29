3c13e58de571c9b8f747617a977b939ede43f6fd
huynn <huynn@spaceaa.com>
2022-03-29 20:08:56 +0700

    up

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+global.title = "Welcome to Adminpage"
+global.token = "http___thememinister.com_token"
+global.is_lock_screen = "http___thememinister.com_is_lock_screen"
+global.is_change_password = "http___thememinister.com_is_change_password"
+global.pages_on_range = 5
+const uat = "http://face-r.icool.com.vn:9094"
+const production = "http://official.icool.com.vn:9095"
+global.uri = uat
+global.avatar_uri = global.uri + "/api/profile/avatars/"
+// global.system_category = '7096052F-0F03-4B2A-A64C-0A3F7D05F925';
+// global.hr_category = '8D659918-2CD3-4468-A22D-47E857DADB9B';
+// global.pr_category = 'B05C1E2C-F002-4979-8E9B-FDC5EDD7C7AE'; //E8E5A325-0239-EB11-80E7-00155DC46400
