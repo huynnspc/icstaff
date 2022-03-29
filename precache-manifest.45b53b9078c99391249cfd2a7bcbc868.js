272c05588ee957a73cf993b06939a314bf1d3e5b
huynn <huynn@spaceaa.com>
2022-03-29 21:04:58 +0700

    Updates

---
+++ precache-manifest.45b53b9078c99391249cfd2a7bcbc868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee11dae5e82650cbc1f006ba1d7183b",
+    "url": "/%7Bicstaff%7D/index.html"
+  },
+  {
+    "revision": "32bfd7fdd20be0a1a2df",
+    "url": "/%7Bicstaff%7D/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "dfb078760a45e52214b9",
+    "url": "/%7Bicstaff%7D/static/css/main.32b87b8b.chunk.css"
+  },
+  {
+    "revision": "32bfd7fdd20be0a1a2df",
+    "url": "/%7Bicstaff%7D/static/js/2.8cee4f76.chunk.js"
+  },
+  {
+    "revision": "8e70745f5ba3847118c205e59b2bcc9e",
+    "url": "/%7Bicstaff%7D/static/js/2.8cee4f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb078760a45e52214b9",
+    "url": "/%7Bicstaff%7D/static/js/main.0739487f.chunk.js"
+  },
+  {
+    "revision": "b4ef7b82f89b9e71b2c1",
+    "url": "/%7Bicstaff%7D/static/js/runtime-main.3df632aa.js"
+  }
+]);
